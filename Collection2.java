d74f37d89d76ff3c86bce4ee6d0ad5767d0698dd
VaishaliSukhdev <105770006+Vaishali451@users.noreply.github.com>
2023-05-07 13:34:06 +0530

    Add files via upload

---
+++ Collection2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Collection1
+{
+    public static void main(String Arg[])
+    {
+        Stack <Integer> lobj= new Stack<Integer>();
+
+        lobj.add(11);
+        lobj.add(21);
+        lobj.add(51);
+        lobj.add(101);
+
+        System.out.println("Element Are:"+lobj);
+
+     
+    }
+}
---
+++ Input.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Input
+{
+  public static void main(String Arg[])
+   {
+    
+     Scanner sobj = new Scanner(System.in);
+  }
+}
